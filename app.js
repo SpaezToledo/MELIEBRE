1ba44ffef2afa2c2d801fb6a23fe4e36c512daad
Santiago Paez Toledo <104175284+santuboca2001@users.noreply.github.com>
2022-06-19 04:10:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 2000;
 
-server.listen(port,()=> console.log('Abriendo el servidor http://localhost:2000'));  
+app.listen(port,()=> console.log('Abriendo el servidor en 2000 port'));  
 
 let port= process.env.PORT || 2000;
 
